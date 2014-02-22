ed75f071b1cdd68e8e51b1b9acea2af1ded99d34
Geoff Lankow <geoff@darktrojan.net>
2014-02-22 15:03:51 +1300

    Add ability to localise install.rdf

--- html/repo/locale/edit.php
+++ html/repo/locale/edit.php
@@ -9,26 +9,44 @@ if (!isset($_GET['file'])) {
 }
 
 $file = $_GET['file'];
-
-$base_strings = get_base_strings();
-$locale_strings = get_locale_strings();
-
 $strings = array();
-foreach ($base_strings as $entity) {
-	$comment = $entity['pre_comment'];
-		if ($comment) {
-		$comment = preg_replace('/\n/', ' ', $comment);
-		$comment = preg_replace('/<!--\s*(.*)\s*-->/', '\1', $comment);
-		$comment = preg_replace('/^#\s*/', '', $comment);
-	}
-	$locale_string = $locale_strings[$entity['key']];
+
+if ($file == 'install.rdf') {
+	$installRDF = new InstallRDF($locale_workdir.'/install.rdf');
 
 	$strings[] = array(
-		'key' => $entity['key'],
-		'comment' => $comment,
-		'base_value' => $entity['value'],
-		'locale_value' => $locale_string
+		'key' => 'name',
+		'comment' => 'The name of the add-on',
+		'base_value' => $installRDF->get_name(),
+		'locale_value' => $installRDF->locale_get_name($locale)
 	);
+	$strings[] = array(
+		'key' => 'description',
+		'comment' => 'The description of the add-on',
+		'base_value' => $installRDF->get_description(),
+		'locale_value' => $installRDF->locale_get_description($locale)
+	);
+
+} else {
+	$base_strings = get_base_strings();
+	$locale_strings = get_locale_strings();
+
+	foreach ($base_strings as $entity) {
+		$comment = $entity['pre_comment'];
+			if ($comment) {
+			$comment = preg_replace('/\n/', ' ', $comment);
+			$comment = preg_replace('/<!--\s*(.*)\s*-->/', '\1', $comment);
+			$comment = preg_replace('/^#\s*/', '', $comment);
+		}
+		$locale_string = $locale_strings[$entity['key']];
+
+		$strings[] = array(
+			'key' => $entity['key'],
+			'comment' => $comment,
+			'base_value' => $entity['value'],
+			'locale_value' => $locale_string
+		);
+	}
 }
 
 require_once 'twig.inc';
--- html/repo/locale/save.php
+++ html/repo/locale/save.php
@@ -9,48 +9,63 @@ if (!$locale_user_is_translator) {
 	exit('not authorized');
 }
 
-$base_strings = get_base_strings();
 $locale_strings = $_POST['locale_strings'];
+if ($file == 'install.rdf') {
+	$installRDF = new InstallRDF($locale_workdir.'/install.rdf');
 
-ob_start();
-if (preg_match('/\.dtd$/', $file)) {
-	$format = '%s%s<!ENTITY %s "%s">%s';
+	if (array_search($locale, $installRDF->get_locales()) !== false) {
+		$installRDF->locale_set_name($locale, $locale_strings['name']);
+		$installRDF->locale_set_description($locale, $locale_strings['description']);
+	} else {
+		$installRDF->locale_add($locale, $locale_strings['name'], $locale_strings['description']);
+	}
+	$installRDF->save();
+
+	$count = 2;
+	$same = 0;
 } else {
-	$format = "%s%s%s = %s\n%s";
-}
+	$base_strings = get_base_strings();
 
-$count = 0;
-$same = 0;
-foreach ($base_strings as $entity) {
-	$locale_value = $locale_strings[$entity['key']];
+	ob_start();
 	if (preg_match('/\.dtd$/', $file)) {
-		$locale_value = htmlspecialchars($locale_value, ENT_COMPAT, 'utf-8');
+		$format = '%s%s<!ENTITY %s "%s">%s';
+	} else {
+		$format = "%s%s%s = %s\n%s";
 	}
 
-	if ($locale_strings[$entity['key']]) {
-		$count++;
-	}
-	if ($entity['value'] == $locale_value) {
-		$same++;
-	}
-	printf(
-		$format,
-		$entity['pre_ws'],
-		$entity['pre_comment'],
-		$entity['key'],
-		$locale_value,
-		$entity['post']
-	);
-}
-$contents = ob_get_contents();
-ob_end_clean();
+	$count = 0;
+	$same = 0;
+	foreach ($base_strings as $entity) {
+		$locale_value = $locale_strings[$entity['key']];
+		if (preg_match('/\.dtd$/', $file)) {
+			$locale_value = htmlspecialchars($locale_value, ENT_COMPAT, 'utf-8');
+		}
 
-if ($db_repo['jetpack']) {
-	file_put_contents($locale_path_abs.'/'.$locale.'.properties', $contents);
-	$file = 'en-US.properties';
-} else {
-	ensure_directory(dirname($file));
-	file_put_contents($locale_path_abs.'/'.$locale.'/'.$file, $contents);
+		if ($locale_strings[$entity['key']]) {
+			$count++;
+		}
+		if ($entity['value'] == $locale_value) {
+			$same++;
+		}
+		printf(
+			$format,
+			$entity['pre_ws'],
+			$entity['pre_comment'],
+			$entity['key'],
+			$locale_value,
+			$entity['post']
+		);
+	}
+	$contents = ob_get_contents();
+	ob_end_clean();
+
+	if ($db_repo['jetpack']) {
+		file_put_contents($locale_path_abs.'/'.$locale.'.properties', $contents);
+		$file = 'en-US.properties';
+	} else {
+		ensure_directory(dirname($file));
+		file_put_contents($locale_path_abs.'/'.$locale.'/'.$file, $contents);
+	}
 }
 
 Files::ReplaceFile($repo, $locale, $file, $count, $same);
